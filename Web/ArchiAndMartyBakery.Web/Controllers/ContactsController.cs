0f679ea0a5158c61575a84fa7459a99e1f7be9d7
Yanko Stoichkov <73684962+YStoichkov@users.noreply.github.com>
2021-09-28 21:50:57 +0300

    Add some functionality

---
+++ Web/ArchiAndMartyBakery.Web/Controllers/ContactsController.cs
@@ -0,0 +1,17 @@
+﻿namespace ArchiAndMartyBakery.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ContactsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+
+        public IActionResult Аbout()
+        {
+            return this.View();
+        }
+    }
+}
---
+++ Web/ArchiAndMartyBakery.Web/Controllers/GalleryController.cs
@@ -0,0 +1,12 @@
+﻿namespace ArchiAndMartyBakery.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class GalleryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
